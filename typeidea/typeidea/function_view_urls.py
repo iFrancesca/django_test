10920d5aa4388af7a91caf4efad63cf738f0ea45
iFrancesca <1158117750@qq.com>
2022-03-11 21:12:02 +0800

    function view

---
+++ typeidea/typeidea/function_view_urls.py
@@ -0,0 +1,33 @@
+"""typeidea URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, re_path
+from django.conf.urls import url
+from .custom_site import custom_site
+
+from blog.views import post_list, post_detail
+from config.views import links
+from typeidea.custom_site import custom_site
+
+urlpatterns = [
+    path('', post_list, name='index'),
+    re_path(r'category/(?P<category_id>\d+)/$', post_list, name='category-list'),
+    re_path(r'tag/(?P<tag_id>\d+)/$', post_list, name='tag-list'),
+    re_path(r'post/(?P<post_id>\d+).html', post_detail, name='post-detail'),
+    re_path(r'links/', links, name='links'),
+    path('super_admin/', admin.site.urls, name='super-admin'),
+    path('admin/', custom_site.urls, name='admin'),
+]
